32bf5d45018df27cb7827f50df566b085764ed4d
Caden Jarrett <caden.jarrett99@gmail.com>
2022-04-24 10:59:33 -0600

    docs: added alphabet containing all lowercased letters and braille counterparts

--- lib/text_to_braille.rb
+++ lib/text_to_braille.rb
@@ -5,6 +5,35 @@ class TexttoBraille < FileTranslate
     @file_path = File.open(ARGV[0], "r")
     @braille_text = File.open(ARGV[1], "w")
     @english_message = @file_path.read.delete("\n")
+    @alphabet = {
+      'a' => ['O.','..','..'],
+      'b' => ['O.','O.','..'],
+      'c' => ['OO','..','..'],
+      'd' => ['OO','.O','..'],
+      'e' => ['O.','.O','..'],
+      'f' => ['OO','O.','..'],
+      'g' => ['OO','OO','..'],
+      'h' => ['O.','OO','..'],
+      'i' => ['.O','O.','..'],
+      'j' => ['.O','OO','..'],
+      'k' => ['O.','..','O.'],
+      'l' => ['O.','O.','O.'],
+      'm' => ['OO','..','O.'],
+      'n' => ['OO','.O','O.'],
+      'o' => ['O.','.O','O.'],
+      'p' => ['OO','O.','O.'],
+      'q' => ['OO','OO','O.'],
+      'r' => ['O.','OO','O.'],
+      's' => ['.O','O.','O.'],
+      't' => ['.O','OO','O.'],
+      'u' => ['O.','..','OO'],
+      'v' => ['O.','O.','OO'],
+      'w' => ['.O','OO','.O'],
+      'x' => ['OO','..','OO'],
+      'y' => ['OO','.O','OO'],
+      'z' => ['O.','.O','OO'],
+      ' ' => ['..','..','..']
+    }
   end
 
   def braille_translate
