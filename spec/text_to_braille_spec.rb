e8dbd139e307b8d21f0036b2eb58ffe3187755c1
Caden Jarrett <caden.jarrett99@gmail.com>
2022-04-25 16:28:43 -0600

    Ref: added test for transpose_sentence

--- spec/text_to_braille_spec.rb
+++ spec/text_to_braille_spec.rb
@@ -25,13 +25,20 @@ RSpec.describe TexttoBraille do
     expect(@english_translator.message_store.join).to eq "hello world this is more than forty characters"
   end
 
-  it "can inpput the braille equivalent to an array" do
+  it "can input the braille equivalent to an array" do
     # binding.pry
     expect(@english_translator.message_translate.join).to eq "O.OO..O..O..O.O.O.O.O.O.O..OO........OOO.OO..OO.O.OOO.O.O.O.OO.O.........OOOO.O.OO...OO....OO.O........OO....OO.O.......OO..O.O..OO.O.OOO.O..O.........OOOO.O.OO..O.....OO.OO.......OOO...O..OO.O.OOO..OOOO.OO.OOO......OO....O.OO..O.....O.OOO.O.....OO.....OOOO.O..O..O.OOO..OO.O."
   end
 
-  it "can translate each character from the message_translate array" do
+  it "can transpose the translated sentences of the braille array" do
     # binding.pry
+    expect(@english_translator.transpose_sentences).to be_a Array
     expect(@english_translator.transpose_sentences).to eq  [[["O.","O.","O.","O.","O.","..",".O","O.","O.","O.","OO","..",".O","O.",".O",".O","..",".O",".O","..","OO","O.","O.","O.","..",".O","O.","O.","OO","..","OO","O.","O.",".O","OO","..","OO","O.","O.","O."],["OO",".O","O.","O.",".O","..","OO",".O","OO","O.",".O","..","OO","OO","O.","O.","..","O.","O.","..","..",".O","OO",".O","..","OO","OO","..",".O","..","O.",".O","OO","OO",".O","..","..","OO","..","OO"],["..","..","O.","O.","O.","..",".O","O.","O.","O.","..","..","O.","..","..","O.","..","..","O.","..","O.","O.","O.","..","..","O.","..","..","O.","..","..","O.","O.","O.","OO","..","..","..","..","O."]],[["O.", "OO", ".O", "O.", "O.", ".O"],["..", "..", "OO", ".O", "OO", "O."],["..", "..", "O.", "..", "O.", "O."]]]
   end
+
+  # it "can translate each character from the message_translate array" do
+  #   @english_translator.transpose_sentences
+  #   # binding.pry
+  #   expect(@english_translator.split_transposed_sentences).to eq [["O.O.O.O.O....OO.O.O.OO...OO..O.O...O.O..OOO.O.O....OO.O.OO..OOO.O..OOO..OOO.O.O.","OO.OO.O..O..OO.OOOO..O..OOOOO.O...O.O......OOO.O..OOOO...O..O..OOOOO.O....OO..OO","....O.O.O....OO.O.O.....O.....O.....O...O.O.O.....O.....O.....O.O.O.OO........O."],["O.OO.OO.O..O", "....OO.OOOO.", "....O...O.O."]]
+  # end
 end
